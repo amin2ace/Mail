97da73ebdb76f3ca427fc3c8e6bec2cc85d80bd3
amin2ace <amin.vmap3240@gmail.com>
2025-03-12 23:11:16 +0330

    add type orm option class in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
 import { UploadModule } from './upload/upload.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import validationSchema from './config/validation-schema.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmOptions } from './config/typeorm-options.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useClass:
+      useClass: TypeOrmOptions,
     }),
     MailModule,
     UploadModule,
