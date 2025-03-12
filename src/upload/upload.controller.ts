e96c99e591e11a8cbf8c695f575844d6ec2d64ac
amin2ace <amin.vmap3240@gmail.com>
2025-03-12 21:14:00 +0330

    Declare the upload controller nodes

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+} from '@nestjs/common';
 import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+
+  @Post('/upload-file')
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {}
+
+  @Post('/upload-files')
+  async uploadFiles(@UploadedFiles() files: Express.Multer.File) {}
+
+  @Post('/retrieve-file/:file-id')
+  async retrieveFile(@Param('file-id') fileId: string) {}
+
+  @Post('/retrieve-files/:user-id')
+  async retrieveFiles(@Param('user-id') userId: string) {}
 }
