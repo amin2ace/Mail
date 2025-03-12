f6bef751dd2e6fd845592346023c617a5620d38a
amin2ace <amin.vmap3240@gmail.com>
2025-03-12 23:23:38 +0330

    Add config module at the beginning

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
 import { UploadModule } from './upload/upload.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import validationSchema from './config/validation-schema.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmOptions } from './config/typeorm-options.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // For recent error it should imported firs of all modules
+      isGlobal: true,
+      validationSchema: validationSchema,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmOptions,
     }),
     MailModule,
     UploadModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: validationSchema,
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
