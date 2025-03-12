2f234dbd6695f194175edcdeb584caa37c163711
amin2ace <amin.vmap3240@gmail.com>
2025-03-12 22:28:49 +0330

    Update upload service interface

--- src/upload/interface/upload-service.interface.ts
+++ src/upload/interface/upload-service.interface.ts
@@ -1,12 +1,14 @@
+import { File } from '../file.entity';
+
 interface IUploadService {
   addFileToDatabase(
     file: Express.Multer.File,
     userId: string,
   ): Promise<{ fileId: string }>;
 
-  retrieveFile(fileId: string, userId: string): Promise<Express.Multer.File>;
+  retrieveFile(fileId: string, userId: string): Promise<File>;
 
-  retrieveFiles(userId: string): Promise<Express.Multer.File[]>;
+  retrieveFiles(userId: string): Promise<File[]>;
 
   checkAccess(fileId: string, userId: string): Promise<Boolean>;
 
